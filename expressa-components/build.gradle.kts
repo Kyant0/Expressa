e8d8fc735282f4b74776096806bc16fab1f2d6d4
Kyant0 <kyant2021@outlook.com>
2025-05-28 14:06:41 +0800

    Rearrange dependencies

--- expressa-components/build.gradle.kts
+++ expressa-components/build.gradle.kts
@@ -49,8 +49,8 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.foundation)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.foundation)
     api(project(":expressa-core"))
 }
