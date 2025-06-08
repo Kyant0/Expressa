a4361628d2511699eec4e9ab1eadb37d52686301
Kyant0 <kyant2021@outlook.com>
2025-06-08 19:39:08 +0800

    [mcu] Update internal API

--- expressa-core/src/main/java/com/kyant/expressa/mcu/DynamicScheme.kt
+++ expressa-core/src/main/java/com/kyant/expressa/mcu/DynamicScheme.kt
@@ -31,80 +31,76 @@ data class DynamicScheme(
     }
 
     fun getMaterialDynamicColor(materialColorRole: MaterialColorRole): Int {
-        return if (nativeHandle != 0L) {
-            nativeGetMaterialDynamicColor(nativeHandle, materialColorRole.ordinal)
-        } else {
-            throw IllegalStateException("DynamicScheme has been freed")
-        }
+        return nativeGetMaterialDynamicColor(nativeHandle, materialColorRole.ordinal)
     }
 
     // primary colors
-    val primary: Int get() = nativeGetPrimary(nativeHandle)
-    val onPrimary: Int get() = nativeGetOnPrimary(nativeHandle)
-    val primaryContainer: Int get() = nativeGetPrimaryContainer(nativeHandle)
-    val onPrimaryContainer: Int get() = nativeGetOnPrimaryContainer(nativeHandle)
+    inline val primary: Int get() = getMaterialDynamicColor(MaterialColorRole.Primary)
+    inline val onPrimary: Int get() = getMaterialDynamicColor(MaterialColorRole.OnPrimary)
+    inline val primaryContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.PrimaryContainer)
+    inline val onPrimaryContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.OnPrimaryContainer)
 
     // secondary colors
-    val secondary: Int get() = nativeGetSecondary(nativeHandle)
-    val onSecondary: Int get() = nativeGetOnSecondary(nativeHandle)
-    val secondaryContainer: Int get() = nativeGetSecondaryContainer(nativeHandle)
-    val onSecondaryContainer: Int get() = nativeGetOnSecondaryContainer(nativeHandle)
+    inline val secondary: Int get() = getMaterialDynamicColor(MaterialColorRole.Secondary)
+    inline val onSecondary: Int get() = getMaterialDynamicColor(MaterialColorRole.OnSecondary)
+    inline val secondaryContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.SecondaryContainer)
+    inline val onSecondaryContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.OnSecondaryContainer)
 
     // tertiary colors
-    val tertiary: Int get() = nativeGetTertiary(nativeHandle)
-    val onTertiary: Int get() = nativeGetOnTertiary(nativeHandle)
-    val tertiaryContainer: Int get() = nativeGetTertiaryContainer(nativeHandle)
-    val onTertiaryContainer: Int get() = nativeGetOnTertiaryContainer(nativeHandle)
+    inline val tertiary: Int get() = getMaterialDynamicColor(MaterialColorRole.Tertiary)
+    inline val onTertiary: Int get() = getMaterialDynamicColor(MaterialColorRole.OnTertiary)
+    inline val tertiaryContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.TertiaryContainer)
+    inline val onTertiaryContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.OnTertiaryContainer)
 
     // error colors
-    val error: Int get() = nativeGetError(nativeHandle)
-    val onError: Int get() = nativeGetOnError(nativeHandle)
-    val errorContainer: Int get() = nativeGetErrorContainer(nativeHandle)
-    val onErrorContainer: Int get() = nativeGetOnErrorContainer(nativeHandle)
+    inline val error: Int get() = getMaterialDynamicColor(MaterialColorRole.Error)
+    inline val onError: Int get() = getMaterialDynamicColor(MaterialColorRole.OnError)
+    inline val errorContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.ErrorContainer)
+    inline val onErrorContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.OnErrorContainer)
 
     // surface colors
-    val surface: Int get() = nativeGetSurface(nativeHandle)
-    val onSurface: Int get() = nativeGetOnSurface(nativeHandle)
-    val surfaceVariant: Int get() = nativeGetSurfaceVariant(nativeHandle)
-    val onSurfaceVariant: Int get() = nativeGetOnSurfaceVariant(nativeHandle)
-    val surfaceContainerHighest: Int get() = nativeGetSurfaceContainerHighest(nativeHandle)
-    val surfaceContainerHigh: Int get() = nativeGetSurfaceContainerHigh(nativeHandle)
-    val surfaceContainer: Int get() = nativeGetSurfaceContainer(nativeHandle)
-    val surfaceContainerLow: Int get() = nativeGetSurfaceContainerLow(nativeHandle)
-    val surfaceContainerLowest: Int get() = nativeGetSurfaceContainerLowest(nativeHandle)
-    val inverseSurface: Int get() = nativeGetInverseSurface(nativeHandle)
-    val inverseOnSurface: Int get() = nativeGetInverseOnSurface(nativeHandle)
+    inline val surface: Int get() = getMaterialDynamicColor(MaterialColorRole.Surface)
+    inline val onSurface: Int get() = getMaterialDynamicColor(MaterialColorRole.OnSurface)
+    inline val surfaceVariant: Int get() = getMaterialDynamicColor(MaterialColorRole.SurfaceVariant)
+    inline val onSurfaceVariant: Int get() = getMaterialDynamicColor(MaterialColorRole.OnSurfaceVariant)
+    inline val surfaceContainerHighest: Int get() = getMaterialDynamicColor(MaterialColorRole.SurfaceContainerHighest)
+    inline val surfaceContainerHigh: Int get() = getMaterialDynamicColor(MaterialColorRole.SurfaceContainerHigh)
+    inline val surfaceContainer: Int get() = getMaterialDynamicColor(MaterialColorRole.SurfaceContainer)
+    inline val surfaceContainerLow: Int get() = getMaterialDynamicColor(MaterialColorRole.SurfaceContainerLow)
+    inline val surfaceContainerLowest: Int get() = getMaterialDynamicColor(MaterialColorRole.SurfaceContainerLowest)
+    inline val inverseSurface: Int get() = getMaterialDynamicColor(MaterialColorRole.InverseSurface)
+    inline val inverseOnSurface: Int get() = getMaterialDynamicColor(MaterialColorRole.InverseOnSurface)
 
     // outline colors
-    val outline: Int get() = nativeGetOutline(nativeHandle)
-    val outlineVariant: Int get() = nativeGetOutlineVariant(nativeHandle)
+    inline val outline: Int get() = getMaterialDynamicColor(MaterialColorRole.Outline)
+    inline val outlineVariant: Int get() = getMaterialDynamicColor(MaterialColorRole.OutlineVariant)
 
     // add-on primary colors
-    val primaryFixed: Int get() = nativeGetPrimaryFixed(nativeHandle)
-    val onPrimaryFixed: Int get() = nativeGetOnPrimaryFixed(nativeHandle)
-    val primaryFixedDim: Int get() = nativeGetPrimaryFixedDim(nativeHandle)
-    val onPrimaryFixedVariant: Int get() = nativeGetOnPrimaryFixedVariant(nativeHandle)
-    val inversePrimary: Int get() = nativeGetInversePrimary(nativeHandle)
+    inline val primaryFixed: Int get() = getMaterialDynamicColor(MaterialColorRole.PrimaryFixed)
+    inline val onPrimaryFixed: Int get() = getMaterialDynamicColor(MaterialColorRole.OnPrimaryFixed)
+    inline val primaryFixedDim: Int get() = getMaterialDynamicColor(MaterialColorRole.PrimaryFixedDim)
+    inline val onPrimaryFixedVariant: Int get() = getMaterialDynamicColor(MaterialColorRole.OnPrimaryFixedVariant)
+    inline val inversePrimary: Int get() = getMaterialDynamicColor(MaterialColorRole.InversePrimary)
 
     // add-on secondary colors
-    val secondaryFixed: Int get() = nativeGetSecondaryFixed(nativeHandle)
-    val onSecondaryFixed: Int get() = nativeGetOnSecondaryFixed(nativeHandle)
-    val secondaryFixedDim: Int get() = nativeGetSecondaryFixedDim(nativeHandle)
-    val onSecondaryFixedVariant: Int get() = nativeGetOnSecondaryFixedVariant(nativeHandle)
+    inline val secondaryFixed: Int get() = getMaterialDynamicColor(MaterialColorRole.SecondaryFixed)
+    inline val onSecondaryFixed: Int get() = getMaterialDynamicColor(MaterialColorRole.OnSecondaryFixed)
+    inline val secondaryFixedDim: Int get() = getMaterialDynamicColor(MaterialColorRole.SecondaryFixedDim)
+    inline val onSecondaryFixedVariant: Int get() = getMaterialDynamicColor(MaterialColorRole.OnSecondaryFixedVariant)
 
     // add-on tertiary colors
-    val tertiaryFixed: Int get() = nativeGetTertiaryFixed(nativeHandle)
-    val onTertiaryFixed: Int get() = nativeGetOnTertiaryFixed(nativeHandle)
-    val tertiaryFixedDim: Int get() = nativeGetTertiaryFixedDim(nativeHandle)
-    val onTertiaryFixedVariant: Int get() = nativeGetOnTertiaryFixedVariant(nativeHandle)
+    inline val tertiaryFixed: Int get() = getMaterialDynamicColor(MaterialColorRole.TertiaryFixed)
+    inline val onTertiaryFixed: Int get() = getMaterialDynamicColor(MaterialColorRole.OnTertiaryFixed)
+    inline val tertiaryFixedDim: Int get() = getMaterialDynamicColor(MaterialColorRole.TertiaryFixedDim)
+    inline val onTertiaryFixedVariant: Int get() = getMaterialDynamicColor(MaterialColorRole.OnTertiaryFixedVariant)
 
     // add-on surface colors
-    val background: Int get() = nativeGetBackground(nativeHandle)
-    val onBackground: Int get() = nativeGetOnBackground(nativeHandle)
-    val surfaceBright: Int get() = nativeGetSurfaceBright(nativeHandle)
-    val surfaceDim: Int get() = nativeGetSurfaceDim(nativeHandle)
-    val scrim: Int get() = nativeGetScrim(nativeHandle)
-    val shadow: Int get() = nativeGetShadow(nativeHandle)
+    inline val background: Int get() = getMaterialDynamicColor(MaterialColorRole.Background)
+    inline val onBackground: Int get() = getMaterialDynamicColor(MaterialColorRole.OnBackground)
+    inline val surfaceBright: Int get() = getMaterialDynamicColor(MaterialColorRole.SurfaceBright)
+    inline val surfaceDim: Int get() = getMaterialDynamicColor(MaterialColorRole.SurfaceDim)
+    inline val scrim: Int get() = getMaterialDynamicColor(MaterialColorRole.Scrim)
+    inline val shadow: Int get() = getMaterialDynamicColor(MaterialColorRole.Shadow)
 
     private companion object {
 
@@ -128,169 +124,5 @@ data class DynamicScheme(
             nativeHandle: Long,
             materialColorRole: Int
         ): Int
-
-        // primary colors
-
-        @FastNative
-        external fun nativeGetPrimary(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnPrimary(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetPrimaryContainer(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnPrimaryContainer(nativeHandle: Long): Int
-
-        // secondary colors
-
-        @FastNative
-        external fun nativeGetSecondary(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnSecondary(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSecondaryContainer(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnSecondaryContainer(nativeHandle: Long): Int
-
-        // tertiary colors
-
-        @FastNative
-        external fun nativeGetTertiary(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnTertiary(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetTertiaryContainer(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnTertiaryContainer(nativeHandle: Long): Int
-
-        // error colors
-
-        @FastNative
-        external fun nativeGetError(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnError(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetErrorContainer(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnErrorContainer(nativeHandle: Long): Int
-
-        // surface colors
-
-        @FastNative
-        external fun nativeGetSurface(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnSurface(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSurfaceVariant(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnSurfaceVariant(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSurfaceContainerHighest(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSurfaceContainerHigh(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSurfaceContainer(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSurfaceContainerLow(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSurfaceContainerLowest(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetInverseSurface(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetInverseOnSurface(nativeHandle: Long): Int
-
-        // outline colors
-
-        @FastNative
-        external fun nativeGetOutline(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOutlineVariant(nativeHandle: Long): Int
-
-        // add-on primary colors
-
-        @FastNative
-        external fun nativeGetPrimaryFixed(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnPrimaryFixed(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetPrimaryFixedDim(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnPrimaryFixedVariant(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetInversePrimary(nativeHandle: Long): Int
-
-        // add-on secondary colors
-
-        @FastNative
-        external fun nativeGetSecondaryFixed(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnSecondaryFixed(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSecondaryFixedDim(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnSecondaryFixedVariant(nativeHandle: Long): Int
-
-        // add-on tertiary colors
-
-        @FastNative
-        external fun nativeGetTertiaryFixed(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnTertiaryFixed(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetTertiaryFixedDim(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnTertiaryFixedVariant(nativeHandle: Long): Int
-
-        // add-on surface colors
-
-        @FastNative
-        external fun nativeGetBackground(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetOnBackground(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSurfaceBright(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetSurfaceDim(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetScrim(nativeHandle: Long): Int
-
-        @FastNative
-        external fun nativeGetShadow(nativeHandle: Long): Int
     }
 }
